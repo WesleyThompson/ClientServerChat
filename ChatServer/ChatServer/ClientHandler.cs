3660c0202a951e4219d79f00b75e478a6d240b65
Wesley Thompson <wesleythompson1995@gmail.com>
2017-04-19 22:24:59 -0500

    WIP client threads

---
+++ ChatServer/ChatServer/ClientHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    public class ClientHandler
+    {
+        TcpClient clientSocket;
+        string clientNumber;
+
+        public ClientHandler()
+        {
+        }
+    }
+}
