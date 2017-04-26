42d6cb3f395e577f1fb78c52a576cbd7c232b56c
Wesley Thompson <wesleythompson1995@gmail.com>
2017-04-26 00:28:44 -0500

    starting server side checks

--- ChatServer/ChatServer/ClientHandler.cs
+++ ChatServer/ChatServer/ClientHandler.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace ChatServer
 {
-    class ClientHandler
+    public class ClientHandler
     {
         TcpClient clientSocket;
         string clNo;
@@ -22,12 +20,21 @@ namespace ChatServer
         private void doChat()
         {
             int requestCount = 0;
-            byte[] bytesFrom = new byte[10025];
+            byte[] bytesFrom = new byte[1024];
             string dataFromClient = null;
-            Byte[] sendBytes = null;
+            Byte[] sendBytes = new byte[1024];
             string serverResponse = null;
             string rCount = null;
             requestCount = 0;
+
+            bool loggedIn = false;
+            String username = null;
+            String password = null;
+
+            Dictionary<string, string> users = new Dictionary<string, string>()
+            {
+                { "Tom" , "Tom11"}
+            };
 
             while (true)
             {
@@ -35,17 +42,47 @@ namespace ChatServer
                 {
                     requestCount = requestCount + 1;
                     NetworkStream networkStream = clientSocket.GetStream();
-                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    networkStream.Read(bytesFrom, 0, 1024);
                     dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
                     dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
                     Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
 
                     rCount = Convert.ToString(requestCount);
-                    serverResponse = "Server to clinet(" + clNo + ") " + rCount;
+
+                    switch(dataFromClient)
+                    {
+                        default:
+                            if(dataFromClient.StartsWith("login "))
+                            {
+                                string loginStuff = RemoveString(dataFromClient, "login ");
+                                ExtractLogin(ref username, ref password, loginStuff);
+
+                                if (username == null || password == null)
+                                {
+                                    serverResponse = "Login missing username or password.";
+                                }
+                                else
+                                {
+                                    
+                                    serverResponse = "Login confirmed.";
+                                }
+                            }
+                            else
+                            {
+                                serverResponse = "Could not understand the command: " + dataFromClient + ".";
+                            }
+                            break;
+                    }
+
+                    
                     sendBytes = Encoding.ASCII.GetBytes(serverResponse);
                     networkStream.Write(sendBytes, 0, sendBytes.Length);
                     networkStream.Flush();
                     Console.WriteLine(" >> " + serverResponse);
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    break;
                 }
                 catch (Exception ex)
                 {
@@ -53,5 +90,46 @@ namespace ChatServer
                 }
             }
         }
+
+        private static string RemoveString(string input, string removal)
+        {
+            int index = input.IndexOf(removal);
+            string message = (index < 0)
+                ? input
+                : input.Remove(index, removal.Length);
+
+            return message;
+        }
+
+        private static void ExtractLogin(ref String userId, ref String password, string loginStuff)
+        {
+            //Error checking
+            string[] stringSeparators = new string[] { " " };
+            string[] inputs = loginStuff.Split(stringSeparators, StringSplitOptions.None);
+
+            if (inputs.Length< 2 || inputs.Length> 2)
+            {
+                Console.WriteLine("Incorrect number of login arguments. Try again.");
+            }
+
+            foreach (string input in inputs)
+            {
+                if (String.IsNullOrEmpty(input) || String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Empty or missing login arguments.");
+                    break;
+                }
+
+                if (userId == null)
+                {
+                    userId = input;
+                }
+                else if (password == null)
+                {
+                    password = input;
+                }
+            }
+        }
+
     }
 }
--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Net.Sockets;
 using System.Net;
 
@@ -10,80 +6,33 @@ namespace ChatServer
 {
     class Program
     {
-        public static void Main()
+        static int port = 12149;
+        static string ipAddress = "127.0.0.1";
+
+        static bool isRunning = true;
+
+        static void Main(string[] args)
         {
-            TcpListener server = null;
-            try
+            TcpListener serverSocket = new TcpListener( IPAddress.Parse(ipAddress), port);
+            TcpClient clientSocket = default(TcpClient);
+
+            serverSocket.Start();
+            Console.WriteLine("Server Started");
+
+            int threadCounter = 0;
+            while (isRunning)
             {
-                // Set the TcpListener on port 13000.
-                Int32 port = 12149;
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-                // TcpListener server = new TcpListener(port);
-                server = new TcpListener(localAddr, port);
-
-                // Start listening for client requests.
-                server.Start();
-
-                // Buffer for reading data
-                Byte[] bytes = new Byte[256];
-                String data = null;
-
-                // Enter the listening loop.
-                while (true)
-                {
-                    Console.Write("Waiting for a connection... ");
-
-                    // Perform a blocking call to accept requests.
-                    // You could also user server.AcceptSocket() here.
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
-
-                    data = null;
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Loop to receive all the data sent by the client.
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        // Translate data bytes to a ASCII string.
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.WriteLine("Received: {0}", data);
-
-                        // Process the data sent by the client.
-                        data = data.ToUpper();
-
-                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                        // Send back a response.
-                        stream.Write(msg, 0, msg.Length);
-                        Console.WriteLine("Sent: {0}", data);
-                    }
-
-                    // Shutdown and end connection
-                    client.Close();
-                }
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
+                threadCounter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Client No: " + threadCounter + " started!");
+                ClientHandler client = new ClientHandler();
+                client.startClient(clientSocket, Convert.ToString(threadCounter));
             }
 
-
-            Console.WriteLine("\nHit enter to continue...");
-            Console.Read();
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> " + "exit");
+            Console.ReadLine();
         }
     }
 }
